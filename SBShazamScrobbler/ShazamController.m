6b01375304562a7b179f71b251c9e4aaffa1342d
Stéphane Bruckert <contact@stephanebruckert.com>
2014-10-10 12:26:06 +0200

    Get last tags only

--- SBShazamScrobbler/ShazamController.m
+++ SBShazamScrobbler/ShazamController.m
@@ -21,9 +21,10 @@
     //Initialize previous session information
     NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
     if ([prefs integerForKey:@"lastTag"] < 0) {
-        [prefs setInteger:0 forKey:@"lastTag"];
+        [prefs setInteger:1 forKey:@"lastTag"];
     };
     
+    //Get the plist file
     NSBundle* bundle = [[NSBundle alloc] initWithPath:PATH];
     NSString* filePath = [bundle pathForResource:FILENAME ofType:EXTENSION];
     
@@ -33,19 +34,18 @@
         NSLog(@"File doesn't exist");
     }
     
-    int dicInARow = 0;
-    bool numberBefore = NO;
-    Song *test;
-    
     //Dictionary of tags
     NSDictionary* plist = [[NSDictionary alloc] initWithContentsOfFile:filePath];
     NSArray* myDic = [plist objectForKey:@"$objects"];
     NSDictionary *obj;
     
-    int i;
-    NSInteger myInt = [prefs integerForKey:@"lastTag"];
-    NSLog(@"my preivous %d", (int)myInt);
-    for (i = ((int)myInt); i < [myDic count]; i++) {
+    Song *test;
+    int i, dicInARow = 0;
+    int lastTag = (int)[prefs integerForKey:@"lastTag"];
+    bool lookForFirstDicAfterStrings = NO;
+    
+    //Iterate through songs data
+    for (i = lastTag; i < [myDic count]; i++) {
         obj = [myDic objectAtIndex:i];
         if ([obj isKindOfClass:[NSString class]]) {
             if (dicInARow >= 2 && dicInARow <= 3) {
@@ -55,29 +55,28 @@
                     [test setSong:[obj description]];
                 }
                 dicInARow++;
+                lookForFirstDicAfterStrings = YES;
             } else {
                 dicInARow = 0;
             }
         }
         if ([obj isKindOfClass:[NSDictionary class]]) {
-            if (numberBefore) {
+            if (lookForFirstDicAfterStrings) {
                 NSTimeInterval timeInterval = [[obj objectForKey:@"NS.time"] doubleValue];
                 NSDate *newDate = [NSDate dateWithTimeIntervalSinceReferenceDate:timeInterval];
                 [test setDate:newDate];
                 NSLog(@"%@", [test description]);
-                numberBefore = NO;
+                lookForFirstDicAfterStrings = NO;
             }
             dicInARow++;
         }
         if ([obj isKindOfClass:[NSNumber class]]) {
             dicInARow = 0;
-            numberBefore = YES;
         }
     }
-    NSLog(@"Last tag: %d", i);
     
-    // saving an NSInteger
-    [prefs setInteger:i forKey:@"lastTag"];
+    // Saving the last tag position
+    [prefs setInteger:(i - 1) forKey:@"lastTag"];
     
 }
 
