a5b3deedc8b8a33dfb792235cdbf2f14838b8c62
Stephane Bruckert <contact@stephanebruckert.com>
2015-09-23 22:54:22 +0200

    renaming project part4

--- Pods/Target Support Files/Pods-SBShazamScrobbler-FMDB/Pods-SBShazamScrobbler-FMDB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SBShazamScrobbler_FMDB : NSObject
-@end
-@implementation PodsDummy_Pods_SBShazamScrobbler_FMDB
-@end
---
+++ Pods/Target Support Files/Pods-ShazamScrobbler/Pods-ShazamScrobbler-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShazamScrobbler : NSObject
+@end
+@implementation PodsDummy_Pods_ShazamScrobbler
+@end
